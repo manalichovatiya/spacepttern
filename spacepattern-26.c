b9948c1b8817caa26098503862c8f4ffe0afc23c
manalichovatiya <115995019+manalichovatiya@users.noreply.github.com>
2023-03-07 21:45:48 +0530

    Add files via upload

---
+++ spacepattern-26.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main (){
+    int a,b,s;
+  for(a=5;a>=1;a--)
+    {
+        for(b=1;b<=a;b++)
+        {
+            printf("%d ",b);
+        }
+        for(s=a;s<5;s++)
+        {
+            printf("  ");
+        }
+        for(s=a;s<5;s++)
+        {
+            printf("  ");
+        }
+        for(b=a;b>=1;b--)
+        {
+            printf("%d ",b);
+        }
+        printf("\n");
+    }
+    return 0;
+}
